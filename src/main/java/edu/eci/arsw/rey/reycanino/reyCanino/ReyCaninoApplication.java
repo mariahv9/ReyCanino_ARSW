bf3cec756cbab62318cce2366db4e47fb945826a
mariahv9 <mafehv1999@hotmail.com>
2020-09-23 18:08:20 -0500

    start

---
+++ src/main/java/edu/eci/arsw/rey/reycanino/reyCanino/ReyCaninoApplication.java
@@ -0,0 +1,11 @@
+package edu.eci.arsw.rey.reycanino.reyCanino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReyCaninoApplication {
+	public static void main (String[] args) {
+		SpringApplication.run (ReyCaninoApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/eci/arsw/rey/reycanino/reyCanino/ReyCaninoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.rey.reycanino.reyCanino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReyCaninoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
